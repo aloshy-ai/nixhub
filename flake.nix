ed6f694346ab591852a847841c015e06ff39b84f
GitHub Action <action@github.com>
2024-12-21 16:48:44 +0400

    Remove unnecessary components and dependencies
    Deleted unused components, configurations, and package files, including `CopyButton`, handler-related files, and `package.json` dependencies. This cleanup reduces project complexity and removes extraneous dependencies to streamline development.

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        # Add default package
         packages.default = pkgs.buildEnv {
           name = "nixhub";
           paths = [ ]; # Empty since this is just a development environment
@@ -21,11 +20,9 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nixfmt-rfc-style
-            nodejs_20
             watchexec
             parallel
             direnv
-            husky
             curl
             just
             git
@@ -34,13 +31,9 @@
           ];
 
           shellHook = ''
-            npm install
             chmod +x ./run
-            chmod -R +x ./scripts/*
-            chmod -R +x ./scripts/tests/*
-            chmod -R +x ./.husky/*
-            chmod -R +x ./.github/*
-            git config core.hooksPath .husky
+            chmod +x ./scripts/*
+            chmod +x ./scripts/tests/*
             echo "░░░░░░░░░░  𝓷𝓲𝔁🅷🆄🅱  ░░░░░░░░░░"
           '';
         };
